7162114767fd1aad9a8bae2e88b2229b9b59352c
Isaiah Diaz <diazisaiah0221@gmail.com>
2018-05-25 15:53:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function prependKitten(){
   return ["Arnold", ...kittens]
 }
 function removeLastKitten(){
-  return kittens.slice(-1)
+  return kittens.slice(-2)
 }
 function removeFirstKitten(){
   return kittens.slice(0)
